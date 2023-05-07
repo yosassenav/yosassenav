bd034560c180d05bf332ade75e17d8a8cfae6d12
yosassenav <mromerovanessa.soy@gmail.com>
2023-05-06 19:01:42 -0500

    attemp of creating a card component for technologies

--- src/components/Techstack/Techstack.js
+++ src/components/Techstack/Techstack.js
@@ -4,6 +4,7 @@ import JSLogo from "../../../public/img/js_logo.png";
 import MongoDBLogo from "../../../public/img/mongodb_logo.jpg";
 import NodejsLogo from "../../../public/img/node_logo.jpg";
 import ExpressLogo from "../../../public/img/expressjs_logo.jpg";
+import Card from "../Card/Card";
 export default function Techstack() {
   return (
     <>
@@ -13,6 +14,7 @@ export default function Techstack() {
         </h2>
       </div>
       <div className="grid grid-cols-3 gap-4 mx-6">
+        <Card props={{ img: { JSLogo }, title: "Javascript" }} />
         <div className="flex flex-col items-center bg-stone-900 hover:bg-gradient-to-r from-sky-600 to-sky-900 rounded">
           <div className="flex justify-center w-32 p-3">
             <Image src={NodejsLogo} alt="" />
@@ -27,14 +29,6 @@ export default function Techstack() {
           </div>
           <div className="flex justify-center p-3">
             <span>React JS</span>
-          </div>
-        </div>
-        <div className="flex flex-col items-center bg-stone-900 hover:bg-gradient-to-r from-sky-600 to-sky-900 rounded">
-          <div className="flex justify-center w-32 p-3">
-            <Image src={JSLogo} alt="" />
-          </div>
-          <div className="flex justify-center p-3">
-            <span>Javascript</span>
           </div>
         </div>
         <div className="flex flex-col items-center bg-stone-900 hover:bg-gradient-to-r from-sky-600 to-sky-900 rounded">
