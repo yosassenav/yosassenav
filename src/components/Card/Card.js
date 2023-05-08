7b5cd77577cf9be35d926b4f20440cdb9eb30b4a
yosassenav <mromerovanessa.soy@gmail.com>
2023-05-07 20:35:50 -0500

    correcting card component to display technologies

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,16 @@
 import Image from "next/image";
 
-export default function Card({ props }) {
-  const { img, title } = props;
-  <>
-    <div className="flex flex-col items-center bg-stone-900 hover:bg-gradient-to-r from-sky-600 to-sky-900 rounded">
-      <div className="flex justify-center w-32 p-3">
-        <Image src={img} alt="" />
+export default function Card({ img, title }) {
+  return (
+    <>
+      <div className="flex flex-col items-center bg-stone-900 hover:bg-gradient-to-r from-sky-600 to-sky-900 rounded">
+        <div className="flex justify-center w-32 p-3">
+          <Image src={img} alt={title} />
+        </div>
+        <div className="flex justify-center p-3">
+          <span>{title}</span>
+        </div>
       </div>
-      <div className="flex justify-center p-3">
-        <span>{title}</span>
-      </div>
-    </div>
-  </>;
+    </>
+  );
 }
--- src/components/Techstack/Techstack.js
+++ src/components/Techstack/Techstack.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import ReactLogo from "../../../public/img/react_logo.jpg";
 import JSLogo from "../../../public/img/js_logo.png";
 import MongoDBLogo from "../../../public/img/mongodb_logo.jpg";
@@ -14,39 +13,11 @@ export default function Techstack() {
         </h2>
       </div>
       <div className="grid grid-cols-3 gap-4 mx-6">
-        <Card props={{ img: { JSLogo }, title: "Javascript" }} />
-        <div className="flex flex-col items-center bg-stone-900 hover:bg-gradient-to-r from-sky-600 to-sky-900 rounded">
-          <div className="flex justify-center w-32 p-3">
-            <Image src={NodejsLogo} alt="" />
-          </div>
-          <div className="flex justify-center p-3">
-            <span>Node JS</span>
-          </div>
-        </div>
-        <div className="flex flex-col items-center bg-stone-900 hover:bg-gradient-to-r from-sky-600 to-sky-900 rounded">
-          <div className="flex justify-center w-32 p-3">
-            <Image src={ReactLogo} alt="" />
-          </div>
-          <div className="flex justify-center p-3">
-            <span>React JS</span>
-          </div>
-        </div>
-        <div className="flex flex-col items-center bg-stone-900 hover:bg-gradient-to-r from-sky-600 to-sky-900 rounded">
-          <div className="flex justify-center w-32 p-3">
-            <Image src={ExpressLogo} alt="" />
-          </div>
-          <div className="flex justify-center p-3">
-            <span>Express JS</span>
-          </div>
-        </div>
-        <div className="flex flex-col items-center bg-stone-900 hover:bg-gradient-to-r from-sky-600 to-sky-900 rounded">
-          <div className="flex justify-center w-32 p-3">
-            <Image src={MongoDBLogo} alt="" />
-          </div>
-          <div className="flex justify-center p-3">
-            <span>Mongo DB</span>
-          </div>
-        </div>
+        <Card img={JSLogo} title="Javascript" />
+        <Card img={ReactLogo} title="ReactJS" />
+        <Card img={NodejsLogo} title="NodeJS" />
+        <Card img={ExpressLogo} title="ExpressJS" />
+        <Card img={MongoDBLogo} title="MongoDB" />
       </div>
     </>
   );
