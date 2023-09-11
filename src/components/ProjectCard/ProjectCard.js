a2b5df69e3246971557b1e620fedbb1512b04b6b
yosassenav <mromerovanessa.soy@gmail.com>
2023-09-11 00:27:37 -0500

    changing site´s complete structure

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,8 @@
+export default function ProjectCard({title, description}){
+    return (
+    <div className="flex flex-col border-solid border-2 border-sky-500 max-w-md p-12">
+        <div>{title}</div>
+        <div>{description}</div>
+        <div>Repository</div>
+    </div>
+)}
