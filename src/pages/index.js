141a53efc1455dc85633889d002fb0575d67c004
yosassenav <mromerovanessa.soy@gmail.com>
2024-01-25 00:04:08 -0500

    attempting to implement auto-scroll using useRef and forwardRef

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 // import ProfilePic from "../../public/img/avatar.svg";
+import { useRef } from 'react';
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import Techstack from "@/components/Techstack/Techstack";
@@ -7,19 +8,27 @@ import WelcomeBanner from "@/components/WelcomeBanner/WelcomeBanner";
 import Circles from "@/components/Circles/Circles";
 
 export default function Home() {
+
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const technologiesRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+
+
   return (
     <>
       <title>Vanessa Martinez Romero</title>
       <Header />
-      <div className="mx-auto w-full sm:w-11/12 md:w-3/4 p-4 area">
+      <div className="mx-auto w-full sm:w-11/12 md:w-3/4 p-4 area" ref={homeRef}>
         <Circles/>
         {/* Home Section */}
-        <WelcomeBanner />
+        <WelcomeBanner ref={aboutRef}/>
         <br/>
         <br/>
 
         {/* Technologies */}
-        <Techstack />
+        <Techstack ref={technologiesRef}/>
        
         {/* Projects */}
         <br/>
@@ -29,7 +38,7 @@ export default function Home() {
         </h2>
         <br/>
         <br/>
-        <div className="flex flex-col lg:flex-row lg:justify-evenly text-center">
+        <div className="flex flex-col lg:flex-row lg:justify-evenly text-center" ref={projectsRef}>
           <ProjectCard title="Cacalli" description="ksjkjdsnckjdsnksd" />
           <ProjectCard
             title="P1"
@@ -38,7 +47,7 @@ export default function Home() {
           <ProjectCard title="P2" description="nkndlcpokpksapoksa" />
         </div>
       </div>
-      <Footer />
+      <Footer ref={contactRef}/>
     </>
   );
 }
